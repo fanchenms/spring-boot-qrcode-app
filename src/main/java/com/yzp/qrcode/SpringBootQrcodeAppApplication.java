6463c9d734a6b8103f9544788915929c4f9e3f6c
yzp <2306807556@qq.com>
2022-08-30 00:07:29 +0800

    init

---
+++ src/main/java/com/yzp/qrcode/SpringBootQrcodeAppApplication.java
@@ -0,0 +1,13 @@
+package com.yzp.qrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrcodeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQrcodeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yzp/qrcode/SpringBootQrcodeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzp.qrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQrcodeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
